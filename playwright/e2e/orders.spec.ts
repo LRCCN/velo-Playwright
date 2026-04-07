04ddd9d420dec44512054c4c759f54f6ff95c16b
Luiz <luizitow@hotmail.com>
2026-04-06 21:35:56 -0400

    starting automation tests for approved request status check

---
+++ playwright/e2e/orders.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('Must check an approved order', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  //Checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  //Checkpoint
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByTestId('search-order-id').fill('VLO-SJIN6D');
+
+  await page.getByTestId('search-order-button').click();
+  
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-SJIN6D');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
